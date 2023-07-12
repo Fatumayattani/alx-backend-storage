8558b3d79c507355dafe5e1aabfa109175ec6fb3
Fatumayattani <fyattani@gmail.com>
2023-07-12 10:45:18 -0700

    save changes

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN `user_id` INT)
+BEGIN 
+    -- Update the average score for the specified user
+    UPDATE users
+    SET average_score = (SELECT AVG(score)
+                        FROM corrections
+                        WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END $$
+DELIMITER ;
